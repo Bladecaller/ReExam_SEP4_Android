747181d8cd137eab321f4fd8d9e4429112ecada5
Bladecaller <aleksievborislav@abv.bg>
2021-08-06 18:02:00 +0300

    list fix

--- app/src/main/java/view/activity/customer/SaunaActivity.java
+++ app/src/main/java/view/activity/customer/SaunaActivity.java
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.example.sep4_android.R;
@@ -29,9 +30,13 @@ public class SaunaActivity extends AppCompatActivity {
     private LineChart graph;
     private SaunaViewModel mSauna;
     private List<DataPoint> datapointList;
-    private List<String> co2,humidity, temperature;
-    private ArrayList<String> xAxis;
-    private ArrayList<String> yAxisCO2,yAxisTemp,yAxisHum;
+    private List<String> co2 = new ArrayList<>();
+    private ArrayList<String> humidity = new ArrayList<>();
+    private ArrayList<String> temperature = new ArrayList<>();
+    private ArrayList<String> xAxis = new ArrayList<>();
+    private ArrayList<String> yAxisCO2 = new ArrayList<>();
+    private ArrayList<String>yAxisTemp = new ArrayList<>();
+    private ArrayList<String>yAxisHum = new ArrayList<>();
     private TextView txtCO2,txtHum,txtTemp;
 
     @Override
@@ -51,11 +56,11 @@ public class SaunaActivity extends AppCompatActivity {
         name = String.valueOf(extras.getInt("Sauna"));
         saunaName.setText("Sauna " + name);
 
-       /* mSauna.getAllDatapointsForASauna(extras.getInt("Sauna")).observe(this, new Observer<List<DataPoint>>() {
+       mSauna.getAllDatapointsForASauna(extras.getInt("Sauna")).observe(this, new Observer<List<DataPoint>>() {
             @Override
             public void onChanged(List<DataPoint> dataPoints) {
                 datapointList = dataPoints;
-
+                System.out.print("ID IS :::::"+extras.getInt("Sauna"));
                 for (int i =0; i < datapointList.size(); i++){
                     xAxis.add(datapointList.get(i).getDateTime());
                     yAxisCO2.add(datapointList.get(i).getCo2());
@@ -65,17 +70,18 @@ public class SaunaActivity extends AppCompatActivity {
                     txtCO2.setText(yAxisCO2.get(0));
                     txtTemp.setText(yAxisTemp.get(0));
                     txtHum.setText(yAxisHum.get(0));
-
-                    *//*yAxisCO2.add(new Entry(Float.parseFloat(xAxis.get(i)),Float.parseFloat(datapointList.get(i).getCo2())));
+                    /*
+                    yAxisCO2.add(new Entry(Float.parseFloat(xAxis.get(i)),Float.parseFloat(datapointList.get(i).getCo2())));
                     yAxisHum.add(new Entry(Float.parseFloat(xAxis.get(i)),Float.parseFloat(datapointList.get(i).getHumidity())));
-                    yAxisTemp.add(new Entry(Float.parseFloat(xAxis.get(i)),Float.parseFloat(datapointList.get(i).getTemperature())));*//*
-
+                    yAxisTemp.add(new Entry(Float.parseFloat(xAxis.get(i)),Float.parseFloat(datapointList.get(i).getTemperature())));
+                    */
                 }
 
             }
-        });*/
+        });
+        /*
 
-        /*ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
 
         LineDataSet datasetCO2 = new LineDataSet(yAxisCO2,"CO2");
         datasetCO2.setDrawCircles(false);
@@ -93,8 +99,8 @@ public class SaunaActivity extends AppCompatActivity {
         dataSets.add(datasetHum);
         dataSets.add(datasetTemp);
 
-        graph.setData(new LineData(dataSets));*/
+        graph.setData(new LineData(dataSets));
 
-
+         */
     }
 }
